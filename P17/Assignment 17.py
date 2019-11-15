69ec24cb46dde8b090c665d83d7e917af062386d
inboxpraveen <inbox.praveen.kumar17@gmail.com>
2019-11-15 21:54:56 +0530

    adding assignment 17_

--- P17/Assignment 17.py
+++ P17/Assignment 17.py
@@ -1,1 +1,102 @@
-from keras.datasets import mnist
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_images(dataset, n_images, samples_per_image):
+    output = np.zeros((32 * n_images, 32 * samples_per_image, 3))
+
+    row = 0
+    for images in dataset.repeat(samples_per_image).batch(n_images):
+        output[:, row*32:(row+1)*32] = np.vstack(images.numpy())
+        row += 1
+
+    plt.figure()
+    plt.imshow(output)
+    plt.show()
+
+def flip(x: tf.Tensor) -> tf.Tensor:
+    """Flip augmentation
+
+    Args:
+        x: Image to flip
+
+    Returns:
+        Augmented image
+    """
+    x = tf.image.random_flip_left_right(x)
+    x = tf.image.random_flip_up_down(x)
+
+    return x
+
+def color(x: tf.Tensor) -> tf.Tensor:
+    """Color augmentation
+
+    Args:
+        x: Image
+
+    Returns:
+        Augmented image
+    """
+    x = tf.image.random_hue(x, 0.08)
+    x = tf.image.random_saturation(x, 0.6, 1.6)
+    x = tf.image.random_brightness(x, 0.05)
+    x = tf.image.random_contrast(x, 0.7, 1.3)
+    return x
+
+def rotate(x: tf.Tensor) -> tf.Tensor:
+    """Rotation augmentation
+
+    Args:
+        x: Image
+
+    Returns:
+        Augmented image
+    """
+
+    return tf.image.rot90(x, tf.random_uniform(shape=[], minval=0, maxval=4, dtype=tf.int32))
+
+def zoom(x: tf.Tensor) -> tf.Tensor:
+    """Zoom augmentation
+
+    Args:
+        x: Image
+
+    Returns:
+        Augmented image
+    """
+
+    # Generate 20 crop settings, ranging from a 1% to 20% crop.
+    scales = list(np.arange(0.8, 1.0, 0.01))
+    boxes = np.zeros((len(scales), 4))
+
+    for i, scale in enumerate(scales):
+        x1 = y1 = 0.5 - (0.5 * scale)
+        x2 = y2 = 0.5 + (0.5 * scale)
+        boxes[i] = [x1, y1, x2, y2]
+
+    def random_crop(img):
+        # Create different crops for an image
+        crops = tf.image.crop_and_resize([img], boxes=boxes, box_ind=np.zeros(len(scales)), crop_size=(32, 32))
+        # Return a random crop
+        return crops[tf.random_uniform(shape=[], minval=0, maxval=len(scales), dtype=tf.int32)]
+
+
+    choice = tf.random_uniform(shape=[], minval=0., maxval=1., dtype=tf.float32)
+
+    # Only apply cropping 50% of the time
+    return tf.cond(choice < 0.5, lambda: x, lambda: random_crop(x))
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+data = (x_train[0:8] / 255).astype(np.float32)
+dataset = tf.data.Dataset.from_tensor_slices(data)
+
+# Add augmentations
+augmentations = [flip, color, zoom, rotate]
+
+for f in augmentations:
+    dataset = dataset.map(lambda x: tf.cond(tf.random_uniform([], 0, 1) > 0.75, lambda: f(x), lambda: x), num_parallel_calls=4)
+dataset = dataset.map(lambda x: tf.clip_by_value(x, 0, 1))
+
+plot_images(dataset, n_images=8, samples_per_image=10)
